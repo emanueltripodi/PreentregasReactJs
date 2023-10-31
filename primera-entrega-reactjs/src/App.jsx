e2ecae2b4bc361a3c5ad4167fd0125809797f3cf
Emanuel Tripodi <emanuelnicolastripodi@gmail.com>
2023-10-31 00:33:35 -0300

    little changes

--- primera-entrega-reactjs/src/App.jsx
+++ primera-entrega-reactjs/src/App.jsx
@@ -3,32 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const titulo = ()=> {
+  return (
+    <div>
+      <h1>Titulo</h1>
+      <h2>Subtitulo</h2>
+    </div>
+
+  )
+}
+
 function App() {
   const [count, setCount] = useState(0)
+  let estiloEnLineaMalaPractica = {
+      color: 'red',
+      fontsize: '50px',
+      backgroundcolor: 'blue'
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  //<div style= { estiloEnLineaMalaPractica } >
+  <div className = 'div' onclick = { ()=>alert( 'bienvenidos') } >
+   
+    {titulo()}
+    
+      <p>EL QUE NO ARRIESGA NO GANA</p>
+     
+    </div>
   )
 }
 
